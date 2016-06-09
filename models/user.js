105d54fef60e45b9e7843bb50e24d74db83a5916
Brian Hague <bhague1281@gmail.com>
2016-06-08 21:32:11 -0700

    created user and unit tests

---
+++ models/user.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    }
+  }, {
+    hooks: {
+      beforeCreate: function(user, options, cb) {
+        var hash = bcrypt.hashSync(user.password, 10);
+        user.password = hash;
+        cb(null, user);
+      }
+    },
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      },
+      toJSON: function() {
+        var user = this.get();
+        delete user.password;
+        return user;
+      }
+    }
+  });
+  return user;
+};
---
+++ test/user.test.js
@@ -0,0 +1,104 @@
+var expect = require('chai').expect;
+var db = require('../models');
+
+before(function(done) {
+  db.sequelize.sync({force:true}).then(function() {
+    done();
+  });
+});
+
+describe('Creating a User', function() {
+  it('should create successfully', function(done) {
+    db.user.create({
+      email: 'test@test.co',
+      name: 'Brian',
+      password: 'password'
+    }).then(function() {
+      done();
+    }).catch(function(error) {
+      done(error);
+    });
+  });
+
+  it('should throw an error on invalid email addresses', function(done) {
+    db.user.create({
+      email: 'test',
+      name: 'Brian',
+      password: 'password'
+    }).then(function(newUser) {
+      done(newUser);
+    }).catch(function(error) {
+      done();
+    });
+  });
+
+  it('should throw an error on invalid name', function(done) {
+    db.user.create({
+      email: 'test@test.co',
+      name: '',
+      password: 'password'
+    }).then(function(newUser) {
+      done(newUser);
+    }).catch(function(error) {
+      done();
+    });
+  });
+
+  it('should throw an error on invalid password', function(done) {
+    db.user.create({
+      email: 'test@test.co',
+      name: 'Brian',
+      password: 'short'
+    }).then(function(newUser) {
+      done(newUser);
+    }).catch(function(error) {
+      done();
+    });
+  });
+
+  it('should hash the password before save', function(done) {
+    db.user.create({
+      email: 'test@test.co',
+      name: 'Brian',
+      password: 'password'
+    }).then(function(newUser) {
+      if (newUser.password === 'password') {
+        done(newUser);
+      } else {
+        done();
+      }
+    }).catch(function(error) {
+      done(error);
+    });
+  });
+});
+
+describe('User instance methods', function() {
+  describe('validPassword', function() {
+    it('should validate a password', function(done) {
+      db.user.findOne().then(function(user) {
+        if (user.validPassword('password')) {
+          done();
+        } else {
+          done(user);
+        }
+      }).catch(function(error) {
+        done(error);
+      })
+    });
+  });
+
+  describe('toJSON', function() {
+    it('should return a user without a password field', function(done) {
+      db.user.findOne().then(function(user) {
+        if (user.toJSON().password === undefined) {
+          done();
+        } else {
+          done(user);
+        }
+      }).catch(function(error) {
+        done(error);
+      })
+    });
+  });
+});
