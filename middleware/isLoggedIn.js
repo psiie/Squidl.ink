b4122903a830b0214e5b6fa9d0f7687edcd8f71d
Brian Hague <bhague1281@gmail.com>
2016-06-08 22:52:50 -0700

    added middleware + bootstrap

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
   if (!req.user) {
-    req.flash('warning', 'You must be logged in to access that page');
+    req.flash('error', 'You must be logged in to access that page');
     res.redirect('/auth/login');
   } else {
     next();
