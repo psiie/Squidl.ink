42ea62a86921fbfa20e6f573a0ad0167faf75925
Brian Hague <bhague1281@gmail.com>
2016-06-08 21:52:15 -0700

    defined passport config

---
+++ config/ppConfig.js
@@ -0,0 +1,30 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var db = require('../models');
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.user.findById(id).then(function(user) {
+    cb(null, user);
+  }).catch(cb);
+});
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+}, function(email, password, cb) {
+  db.user.find({
+    where: { email: email }
+  }).then(function(user) {
+    if (!user || !user.validPassword(password)) {
+      cb(null, false);
+    } else {
+      cb(null, user);
+    }
+  }).catch(cb);
+}));
+
+module.exports = passport;
